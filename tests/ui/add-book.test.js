76845298df8aa6f123bdda2f4e4f8c7760c272c9
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-06-28 12:06:07 +0300

    UI tests URL updated

--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-book');
+    await page.goto('https://helixs66-devopsexamprep-5.onrender.com/add-book');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('http://localhost:8080/books');
+    await page.goto('https://helixs66-devopsexamprep-5.onrender.com/books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
